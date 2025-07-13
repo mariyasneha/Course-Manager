0df347d66bdde405a1122573bb60762776b3d441
mariyasneha <mariyasneha104@gmail.com>
2025-07-13 19:59:12 +0530

    Second Message

--- src/component/ViewAllCourses.jsx
+++ src/component/ViewAllCourses.jsx
@@ -17,7 +17,7 @@ const ViewAllCourses = () => {
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <table class="table">
+                <table class="table table-bordered">
                   <thead>
                     <tr>
                       <th scope="col">Id</th>
